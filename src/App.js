d3683f7e65211c7f56091e2b7ea3893e8fea1885
JoaoGracindo <gabrielpessoa97@gmail.com.com>
2022-09-23 18:54:25 -0300

    pre-code

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {useState} from 'react';
+import styled from 'styled-components';
+
+import forca0 from './assets/forca0.png';
+import forca1 from './assets/forca1.png';
+import forca2 from './assets/forca2.png';
+import forca3 from './assets/forca3.png';
+import forca4 from './assets/forca4.png';
+import forca5 from './assets/forca5.png';
+import forca6 from './assets/forca6.png';
+
+import palavras from './palavras';
+import alfabeto from './alfabeto';
+
+function App (){
+    return (
+
+    )
+}
+
+
+
+const Teclado = styled.div`
+    display: flex;
+    justify-content:space-between;
+    align-items: center;
+    
+`
